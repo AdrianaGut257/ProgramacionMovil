45cc832fce482f5aa30a86fcf15123ab57445908
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2025-09-10 15:21:44 -0400

    Feat: Add new fuction about  individual registration

---
+++ lib/features/presentation/pages/individual_mode/widgets/players_register_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:programacion_movil/config/colors.dart';
+import '../../../widgets/Inputs/player_input_field.dart';
+import '../../../widgets/buttons/custom_button.dart';
+
+class PlayersRegisterScreen extends StatefulWidget {
+  const PlayersRegisterScreen({super.key});
+
+  @override
+  State<PlayersRegisterScreen> createState() => _PlayersRegisterScreenState();
+}
+
+class _PlayersRegisterScreenState extends State<PlayersRegisterScreen> {
+  // Lista de jugadores 
+  List<String> players = [""];
+
+  void _addPlayer() {
+    setState(() {
+      players.add("");
+    });
+  }
+
+  void _removePlayer(int index) {
+    setState(() {
+      if (players.length > 1) {
+        players.removeAt(index);
+      }
+    });
+  }
+
+  void _updatePlayer(int index, String value) {
+    setState(() {
+      players[index] = value;
+    });
+  }
+
+  void _startGame() {
+    final validPlayers = players.where((p) => p.trim().isNotEmpty).toList();
+
+    if (validPlayers.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Agrega al menos un jugador")),
+      );
+      return;
+    }
+
+    debugPrint("Jugadores: $validPlayers");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+                 Row(
+                children: [
+                  IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+                ],
+              ),
+            const SizedBox(height: 20),
+
+                // Title
+              const Text(
+                'StopWord',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.primary,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "Ingresar nombres",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: AppColors.textSecondary,
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // Lista dinámica de campos
+              Expanded(
+                child: ListView.builder(
+                  itemCount: players.length,
+                  itemBuilder: (context, index) {
+                    return PlayerInputField(
+                      index: index,
+                      isLast: index == players.length - 1,
+                      onChanged: (value) => _updatePlayer(index, value),
+                      onAdd: _addPlayer,
+                      onRemove: () => _removePlayer(index),
+                    );
+                  },
+                ),
+              ),
+
+            
+               const SizedBox(height: 20),
+
+              // Botón jugar reutilizando CustomButton
+              CustomButton(text: "Jugar 🎮", onPressed: _startGame),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
