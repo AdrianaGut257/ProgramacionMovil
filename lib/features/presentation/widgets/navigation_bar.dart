829ecdc8c06b6b7f914a16b7d35f792d258eb5bc
AdrianaGut257 <adrianagutierrezmollo@gmail.com>
2025-09-10 13:20:06 -0400

    xd

--- lib/features/presentation/widgets/navigation_bar.dart
+++ lib/features/presentation/widgets/navigation_bar.dart
@@ -9,7 +9,7 @@ class CustomNavigationBar extends StatelessWidget {
         color: Colors.white,
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
+            color: Colors.grey.withValues(alpha: 0.3),
             spreadRadius: 1,
             blurRadius: 5,
           ),
