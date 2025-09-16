0b40e02e411ade0a7725c5f363b0f1f9627c3a55
AdrianaGut257 <adrianagutierrezmollo@gmail.com>
2025-09-16 10:12:01 -0400

    avances de la mostrar la informacion

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -3,6 +3,10 @@ import 'package:programacion_movil/features/presentation/pages/category/category
 import 'package:programacion_movil/features/presentation/pages/individual_mode/widgets/players_register_screen.dart';
 import '../features/presentation/pages/home/home_page.dart';
 import '../features/presentation/pages/group_mode/group_mode_page.dart';
+import '../features/presentation/pages/modality_selection/modality_selection_page.dart';
+import '../features/presentation/pages/modality_information/hard_mode.dart';
+import '../features/presentation/pages/modality_information/normal_mode.dart';
+import '../features/presentation/pages/modality_information/team_mode.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
@@ -12,13 +16,29 @@ class AppRouter {
         path: '/group-mode',
         builder: (context, state) => const GroupModePage(),
       ),
-       GoRoute(
+      GoRoute(
         path: '/player-register',
         builder: (context, state) => const PlayersRegisterScreen(),
       ),
       GoRoute(
         path: '/select-categories',
         builder: (context, state) => const Category(),
+      ),
+      GoRoute(
+        path: '/modality-selection',
+        builder: (context, state) => const ModalitySelectionPage(),
+      ),
+      GoRoute(
+        path: '/modality-information-hard',
+        builder: (context, state) => const HardModePage(),
+      ),
+      GoRoute(
+        path: '/modality-information-normal',
+        builder: (context, state) => const NormalModePage(),
+      ),
+      GoRoute(
+        path: '/modality-information-team',
+        builder: (context, state) => const TeamModePage(),
       ),
     ],
   );
