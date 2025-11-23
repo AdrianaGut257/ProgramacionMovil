b086cd7e22c1c336e24626e8e8650a9049cadf4c
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2025-11-22 23:29:19 -0400

    arreglo

--- lib/features/presentation/widgets/navigation_bar.dart
+++ lib/features/presentation/widgets/navigation_bar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CustomNavigationBar extends StatelessWidget {
+  const CustomNavigationBar({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
